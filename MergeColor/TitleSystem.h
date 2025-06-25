aeb63a490aa8de16313fd6621ff7ab9043362afc
arke0909 <arke0909@gmail.com>
2025-06-26 00:29:38 +0900

    Fix : TitleSystem

--- MergeColor/TitleSystem.h
+++ MergeColor/TitleSystem.h
@@ -1,6 +1,4 @@
 ﻿#pragma once
-#include<io.h>
-#include<fcntl.h>
 #include "KeyController.h"
 #include "Console.h"
 
@@ -14,6 +12,5 @@ public:
 	// 키 입력 받아서 선택된 스테이지 반환
 	Stage GetCurSelectStage();
 	// 애니메이션 다 실행시켜주는 함수
-	void EnterAnimation();
-	void SpiralAnimation(COORD _resolution, int _delaytime);
+	
 };
