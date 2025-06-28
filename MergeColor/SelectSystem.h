12de13acad4b1a8164e3948ecf61381ab0a85f35
arke0909 <arke0909@gmail.com>
2025-06-28 19:17:44 +0900

    Fix : TitleSystem

---
+++ MergeColor/SelectSystem.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "KeyController.h"
+#include "ClearInfoManager.h"
+#include "Console.h"
+using std::max;
+using std::min;
+using std::clock;
+#undef max;
+#undef min;
+
+class SelectSystem
+{
+private:
+	int _stageCnt = 0;
+public:
+	SelectSystem(int stageCnt) : _stageCnt(stageCnt) {}
+
+	InTitleSelect GetCurSelect();
+	// 키 입력 받아서 선택된 스테이지 반환
+	Stage GetCurSelectStage();
+};
+	
